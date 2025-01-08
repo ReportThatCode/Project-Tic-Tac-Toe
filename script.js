a203dfb178a3c0f89a1af728d756d1d2a76dc28c
ReportThatCode <valentin_aaron_rabetini@hotmail.com>
2025-01-08 14:14:27 -0300

    finish console game with 2 players or players vs machine

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const gameBoard = (function(){
     let isPlaying = false;
     let mode = 1;
     const table = [
-        "null","x","null",
+        "null","null","null",
         "null","null","null",
         "null","null","null"
     ];
 
     startGame = ()=>{
-        displayController.movePlayer();
         gameBoard.isPlaying = true;
+        displayController.movePlayer();
     }
     resetGame = ()=>{
         console.log("reiniciando el juego")
@@ -25,7 +25,9 @@ const gameBoard = (function(){
         table,
         startGame,
         reset: resetGame,
-        stopGame
+        stopGame,
+        isPlaying,
+        mode
 
     }
 })();
@@ -45,7 +47,6 @@ const displayController = (function(){
         [0, 4, 8], // Diagonal principal
         [2, 4, 6], // Diagonal inversa
       ];
-
       
     const showWinner = (mark)=>{
         resetGame();
@@ -67,22 +68,31 @@ const displayController = (function(){
     }
 
     const getTurn = ()=>{
-        console.log("le toca a => "+ playersTurn[turn])
         return playersTurn[turn];
     }
 
     const finishTurn = ()=>{
         turn = (turn + 1) % playersTurn.length;
-        console.log(turn)
     }
 
-     
     const markIndex = (index,mark) => {
         gameBoard.table[index] = mark
     }
 
+    const getIndex = (mark)=>{
+        let index;
+        if(mark === "o" && gameBoard.mode === 1){
+              index = numberAleatorio();
+              console.log("Esta jugando IA");
+              return index
+        } 
+        else {
+            index = prompIndex(`Turno de ${mark}, Elija una casilla`);
+            return index
+        }
+    }
 
-    const prompIndex = (prompText = "elija un numero del 1 al 9") => {
+    const prompIndex = (prompText = "elija una casilla") => {
         let index = prompt(prompText)
         isAvialable = displayController.indexIsDisabled(index);
         if(!isAvialable){
@@ -91,16 +101,14 @@ const displayController = (function(){
         else {
             return index
         }
-
     }
 
     const numberAleatorio = ()=> {
         let index = Math.floor(Math.random() * 9);
+        if(!indexIsDisabled(index)){
+            return numberAleatorio()
+        }
         return index;
-    }
-
-    const check__IA__index = ()=>{
-
     }
 
     const indexIsDisabled = (index)=>{
@@ -129,7 +137,8 @@ const displayController = (function(){
         finishTurn,
         movePlayer,
         prompIndex,
-        numberAleatorio
+        numberAleatorio,
+        getIndex
     }
 
 }())
@@ -149,20 +158,14 @@ const player = (player,mark)=>{
             },
 
             moveTable(){
-                let index;
-            
                 if (gameBoard.isPlaying === false) return
-                    
-                else if(gameBoard.mode = 1 && mark === "o"){
-                    index = numberAleatorio();
-                    isAvialable = displayController.indexIsDisabled(index)
+                // hacer una funcion que devuelta el index segun si es bot o player
+                   let index = displayController.getIndex(mark)
 
-                    
-
-                } else {
-                    index = displayController.prompIndex(`Turno de ${mark}, elija un numero entre 1 al 9`);    
                     displayController.markIndex(index,mark);
+
                     console.log(displayController.showTable())
+
                     const isWinner = displayController.isWinner(mark)
 
                     if(isWinner === true) {
@@ -171,7 +174,7 @@ const player = (player,mark)=>{
                         displayController.finishTurn();
                         displayController.movePlayer();
                     }
-                }
+                
             }
         }
 
@@ -179,12 +182,3 @@ const player = (player,mark)=>{
 
 const playerX = player("Player1","x");
 const playerO = player("Player2","o");
-
-
-
-
-
-
-//console.log(gameBoard)
-//playerX.moveTable() 
-//playerO.moveTable()
